3d726fa6d42a736dd1b14d13dc8acf4bb467ed08
imrajat24 <imrajatgulati@gmail.com>
2022-05-22 21:32:20 +0530

    added the header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,34 @@
-import Nav from "./Navigation";
-
+import imgHardware from "../images/hardware-card.png";
+import imgSteel from "../images/steel-card.png";
+import whatsAppImg from "../images/whatsapp.png";
 const Header = () => {
   return (
     <div className="header">
-      <div className="navigation">
-        <Nav />
+      <div className="header_content row">
+        <div className="header_content-heading">
+          <h1>One-Stop Solution for all the Hardware & Steel requirements</h1>
+          <h3>
+            India’s finest distributors of hardware & steel products that
+            support industrial houses and manufacturers for all their
+            requirements.
+          </h3>
+        </div>
+        <div className="header_content-images">
+          <a to="/">
+            <img src={imgHardware} alt="hardware" />
+          </a>
+          <a to="/">
+            <img src={imgSteel} alt="steel" />
+          </a>
+        </div>
+      </div>
+      <div className="whatsApp-btn">
+        <a
+          href="https://api.whatsapp.com/send?phone=3197010240285"
+          target="_blank"
+        >
+          <img src={whatsAppImg} alt="whatsApp-btn" />
+        </a>
       </div>
     </div>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,12 @@
 import Header from "../components/Header";
+import Nav from "../components/Navigation";
+
 const HomePage = () => {
   return (
     <div>
+      <div className="navigation">
+        <Nav />
+      </div>
       <Header />
     </div>
   );
