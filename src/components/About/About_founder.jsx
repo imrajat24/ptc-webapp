613394e675cbc16a1bdf393a81b4c461bf5cc2f7
imrajat24 <imrajatgulati@gmail.com>
2022-06-23 19:25:07 +0530

    completed the styling of the about section

--- src/components/About/About_founder.jsx
+++ src/components/About/About_founder.jsx
@@ -6,16 +6,12 @@ const About_founder = () => {
       <div className="about-founder_img">
         <img src={founder} alt="founder_image" />
       </div>
-      <div className="about_founder_content sec_cta-content">
-        <div className="upper-triangle"></div>
-        <div className="lower-triangle"></div>
-        <div className="text">
-          <p>
-            “A rule that every industrialist must follow; <br /> Make the best
-            quality of goods possible at the lowest cost possible, paying the
-            highest wages possible.”
-          </p>
-        </div>
+      <div className="about-founder_content sec_cta-content">
+        <p>
+          “A rule that every industrialist must follow; <br /> Make the best
+          quality of goods possible at the lowest cost possible, paying the
+          highest wages possible.”
+        </p>
       </div>
     </section>
   );
