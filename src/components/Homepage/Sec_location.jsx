eba09eaa12e33ef558f8de3a168e7c6474430e3f
imarajat24 <imrajatgulati@gmail.cometch = +refs/heads/*:refs/remotes/origin/*>
2023-05-22 22:55:38 +0530

    rectified the map location

--- src/components/Homepage/Sec_location.jsx
+++ src/components/Homepage/Sec_location.jsx
@@ -9,7 +9,7 @@ const Sec_location = () => {
       </div>
       <div className="mapouter sec_location-map">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d56268.015996759685!2d76.78815378755986!3d28.184487166156693!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d37f869999489%3A0xdc24c0dd31c27f96!2sPUNIT%20TRADING%20COMPANY!5e0!3m2!1sen!2sin!4v1654331530131!5m2!1sen!2sin"
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3516.4956085860317!2d76.85452033366259!3d28.19225127200848!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d37b955555555%3A0xf6a7812e5bd0de32!2sPunit%20Trading%20Company!5e0!3m2!1sen!2sin!4v1684776159798!5m2!1sen!2sin"
           loading="lazy"
         ></iframe>
       </div>
